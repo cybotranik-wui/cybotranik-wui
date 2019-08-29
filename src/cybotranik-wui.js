2169b9fc455ae993cc7c91d371be04d94e33a5e4
Azmi ŞAHİN <azmisahin@outlook.com>
2019-08-29 15:52:37 +0300

    Clasic Javascript

---
+++ src/cybotranik-wui.js
@@ -0,0 +1,14 @@
+/**
+ * Cybotranik WUI
+ *
+ * Website html User Interface.
+ *
+ * @author Azmi SAHIN
+ * @since 2019
+ * */
+function CybotranikWUI() { }
+
+/**
+ * Initalize
+ */
+new CybotranikWUI()
