160a6752482284428fe1576b1bb6f93ba27b41c9
Azmi ŞAHİN <azmisahin@outlook.com>
2019-08-30 00:08:23 +0300

    HTML 5 element, html element and custom element

--- src/cybotranik-wui.js
+++ src/cybotranik-wui.js
@@ -107,19 +107,19 @@ CybotranikWUI.prototype.documentAppendCss = function (syntax) {
 
   switch (this.currentBrowser()) {
 
-  // Old Property
-  case 'MSIE 5': element.styleSheet.cssText = syntax; break
-  case 'MSIE 7': element.styleSheet.cssText = syntax; break
-  case 'MSIE 8': element.styleSheet.cssText = syntax; break
+    // Old Property
+    case 'MSIE 5': element.styleSheet.cssText = syntax; break
+    case 'MSIE 7': element.styleSheet.cssText = syntax; break
+    case 'MSIE 8': element.styleSheet.cssText = syntax; break
 
     // New Property
-  case 'MSIE 9': element.innerText = syntax; break
-  case 'MSIE 10': element.innerText = syntax; break
-  case 'IE 11': element.innerText = syntax; break
-  case 'Chrome 76': element.innerText = syntax; break
-  case 'Edge 18': element.innerText = syntax; break
-  case 'Firefox 68': element.innerText = syntax; break
-  default: element.innerText = syntax
+    case 'MSIE 9': element.innerText = syntax; break
+    case 'MSIE 10': element.innerText = syntax; break
+    case 'IE 11': element.innerText = syntax; break
+    case 'Chrome 76': element.innerText = syntax; break
+    case 'Edge 18': element.innerText = syntax; break
+    case 'Firefox 68': element.innerText = syntax; break
+    default: element.innerText = syntax
   }
 
   // by default it will be at the bottom of the title.
@@ -153,22 +153,73 @@ CybotranikWUI.prototype.Size = function (value) {
   var factor = 14
 
   switch (this.currentBrowser()) {
-  case 'MSIE 5': result = (factor * 1.05) * value + 'px'; break
-  case 'MSIE 7': result = (factor * 1.05) * value + 'px'; break
-  case 'MSIE 8': result = (factor * 1.05) * value + 'px'; break
-  case 'MSIE 9': result = (factor * 1.05) * value + 'px'; break
+    case 'MSIE 5': result = (factor * 1.05) * value + 'px'; break
+    case 'MSIE 7': result = (factor * 1.05) * value + 'px'; break
+    case 'MSIE 8': result = (factor * 1.05) * value + 'px'; break
+    case 'MSIE 9': result = (factor * 1.05) * value + 'px'; break
 
     // rem compatible.
-  case 'MSIE 10': result = value + 'rem'; break
-  case 'MSIE 11': result = value + 'rem'; break
-  case 'Chrome 76': result = value + 'rem'; break
+    case 'MSIE 10': result = value + 'rem'; break
+    case 'MSIE 11': result = value + 'rem'; break
+    case 'Chrome 76': result = value + 'rem'; break
 
-  case 'Edge 18': result = (factor * 1.05) * value + 'px'; break
-  case 'Firefox 68': result = (factor * 1.05) * value + 'px'; break
+    case 'Edge 18': result = (factor * 1.05) * value + 'px'; break
+    case 'Firefox 68': result = (factor * 1.05) * value + 'px'; break
 
     // rem compatible default.
-  default: result = value + 'rem'
+    default: result = value + 'rem'
   }
 
   return result
+}
+
+/**
+ * HTML 5 element, html element and custom element tags
+ * 
+ */
+CybotranikWUI.prototype.HtmlElements = ''
+
+  // Content sectioning
+  + 'address,article,aside,footer,header,h1,h2,h3,h4,h5,h6,hgroup,main,nav,section'
+
+  // Text content
+  + ',blockquote,dd,div,dl,dt,figcaption,figure,hr,li,ol,p,pre,ul'
+
+  // Inline text semantics
+  + ',a,abbr,b,bdi,bdo,br,cite,code,data,dfn,em,i,kbd,mark,q,rb,rp,rt,rtc,ruby,s,samp,small,span,strong,sub,sup,time,u,var,wbr'
+
+  // Image and multimedia
+  + ',area,audio,img,track,video'
+
+  // Embedded content
+  + ',embed,iframe,noembed,object,param,picture,source'
+
+  // Demarcating edits
+  + ',del,ins'
+
+  // Table content
+  + ',caption,col,colgroup,table,tbody,td,tfoot,th,thead,tr'
+
+  // Forms
+  + ',button,datalist,form,input,label,legend,meter,optgroup,option,output,progress,select,textarea'
+
+  // Interactive elements
+  + ',details,dialog,summary'
+
+  // Web Components
+  + ',shadow,template'
+
+  + ''.split(',')
+
+/**
+ * Document Add current HTML Elements array
+ */
+CybotranikWUI.prototype.createElementArray = function () {
+
+  var array = this.HtmlElements()
+
+  for (var index = 0; index < array.length; index++) {
+    var element = array[index]
+    document.createElement(element)
+  }
 }
