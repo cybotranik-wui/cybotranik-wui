25cceb43537acc1bbcb0a8a6a9b3679d2460f0f9
Azmi ŞAHİN <azmisahin@outlook.com>
2019-08-29 22:58:30 +0300

    Current navigator device name and version

--- src/cybotranik-wui.js
+++ src/cybotranik-wui.js
@@ -9,6 +9,55 @@
 function CybotranikWUI() { }
 
 /**
+ * Current navigator device name and version
+ * 
+ * @returns {string} Navigator Name and Version
+ */
+CybotranikWUI.prototype.currentBrowser = function () {
+
+  var userAgent =
+    navigator.userAgent
+    , compatibility
+    , userAgentMatch = userAgent
+      .match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []
+
+  // IE Test
+  if (/trident/i.test(userAgentMatch[1])) {
+
+    // Compatibility
+    compatibility = /\brv[ :]+(\d+)/g.exec(userAgent) || []
+
+    // Compatibility Short Name
+    return 'IE ' + (compatibility[1] || '')
+  }
+
+  // Chrome Test
+  if (userAgentMatch[1] === 'Chrome') {
+
+    // Compatibility
+    compatibility = userAgent.match(/\b(OPR|Edge?)\/(\d+)/)
+
+    if (compatibility !== null)
+      return compatibility
+        .slice(1).join(' ')
+        .replace('OPR', 'Opera')
+        .replace('Edg ', 'Edge ')
+  }
+
+  // Compatibility Short Name
+  userAgentMatch = userAgentMatch[2] ? [userAgentMatch[1]
+    , userAgentMatch[2]] : [navigator.appName, navigator.appVersion, '-?']
+
+  // Finaly
+  if ((compatibility = userAgent.match(/version\/(\d+)/i)) != null)
+    userAgentMatch.splice(1, 1, compatibility[1])
+
+  // Return
+  return userAgentMatch.join(' ')
+
+}
+
+/**
  * CSS Property string generate syntax
  * 
  * @param {string} property Element Property
